3e8633833f5da2c3ec8868902672ac6ae094a30a
ghofranbenhassen <ghofranebenhassen64@gmail.com>
2025-04-09 13:04:34 +0100

    post and formulaire im gonna do migrate again

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,16 +3,17 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+
 use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
+use App\Entity\Commentaire;
 
 #[ORM\Entity]
 class User
 {
+
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(name: "id_user", type: "integer")]
-    private ?int $id = null;
+    #[ORM\Column(type: "integer")]
+    private int $id_user;
 
     #[ORM\Column(type: "string", length: 255)]
     private string $nom;
@@ -20,20 +21,100 @@ class User
     #[ORM\Column(type: "string", length: 255)]
     private string $prenom;
 
-    #[ORM\Column(type: "string", length: 255, unique: true)]
+    #[ORM\Column(type: "string", length: 255)]
     private string $email;
 
     #[ORM\Column(type: "string", length: 255)]
-    private string $motDePasse;
+    private string $mot_de_passe;
 
-    #[ORM\Column(type: "string", length: 20)]
-    private string $telephone;
+    #[ORM\Column(type: "integer")]
+    private int $telephonne;
 
     #[ORM\Column(type: "datetime")]
-    private \DateTimeInterface $dateCreationCompte;
+    private \DateTimeInterface $date_creation_compte;
 
-    #[ORM\Column(type: "text", nullable: true)]
-    private ?string $photoProfil = null;
+    #[ORM\Column(type: "text")]
+    private string $photo_profil;
+
+    public function getId_user()
+    {
+        return $this->id_user;
+    }
+
+    public function setId_user($value)
+    {
+        $this->id_user = $value;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($value)
+    {
+        $this->nom = $value;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($value)
+    {
+        $this->prenom = $value;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($value)
+    {
+        $this->email = $value;
+    }
+
+    public function getMot_de_passe()
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function setMot_de_passe($value)
+    {
+        $this->mot_de_passe = $value;
+    }
+
+    public function getTelephonne()
+    {
+        return $this->telephonne;
+    }
+
+    public function setTelephonne($value)
+    {
+        $this->telephonne = $value;
+    }
+
+    public function getDate_creation_compte()
+    {
+        return $this->date_creation_compte;
+    }
+
+    public function setDate_creation_compte($value)
+    {
+        $this->date_creation_compte = $value;
+    }
+
+    public function getPhoto_profil()
+    {
+        return $this->photo_profil;
+    }
+
+    public function setPhoto_profil($value)
+    {
+        $this->photo_profil = $value;
+    }
 
     #[ORM\OneToMany(mappedBy: "user", targetEntity: Posts::class)]
     private Collection $posts;
@@ -41,260 +122,38 @@ class User
     #[ORM\OneToMany(mappedBy: "user", targetEntity: Commentaire::class)]
     private Collection $commentaires;
 
-    #[ORM\OneToMany(mappedBy: "user", targetEntity: Admin::class)]
-    private Collection $admins;
-
-    #[ORM\OneToMany(mappedBy: "user", targetEntity: Conducteur::class)]
-    private Collection $conducteurs;
-
-    #[ORM\OneToMany(mappedBy: "user", targetEntity: Paiement::class)]
-    private Collection $paiements;
-
-    #[ORM\OneToMany(mappedBy: "user", targetEntity: Reservation::class)]
-    private Collection $reservations;
-
-    public function __construct()
-    {
-        $this->posts = new ArrayCollection();
-        $this->commentaires = new ArrayCollection();
-        $this->admins = new ArrayCollection();
-        $this->conducteurs = new ArrayCollection();
-        $this->paiements = new ArrayCollection();
-        $this->reservations = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getNom(): string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
-        return $this;
-    }
-
-    public function getPrenom(): string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
-        return $this;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function getMotDePasse(): string
-    {
-        return $this->motDePasse;
-    }
-
-    public function setMotDePasse(string $motDePasse): self
-    {
-        $this->motDePasse = $motDePasse;
-        return $this;
-    }
-
-    public function getTelephone(): string
-    {
-        return $this->telephone;
-    }
-
-    public function setTelephone(string $telephone): self
-    {
-        $this->telephone = $telephone;
-        return $this;
-    }
-
-    public function getDateCreationCompte(): \DateTimeInterface
-    {
-        return $this->dateCreationCompte;
-    }
-
-    public function setDateCreationCompte(\DateTimeInterface $dateCreationCompte): self
-    {
-        $this->dateCreationCompte = $dateCreationCompte;
-        return $this;
-    }
-
-    public function getPhotoProfil(): ?string
-    {
-        return $this->photoProfil;
-    }
-
-    public function setPhotoProfil(?string $photoProfil): self
-    {
-        $this->photoProfil = $photoProfil;
-        return $this;
-    }
 
     public function getPosts(): Collection
     {
         return $this->posts;
     }
-
+    
     public function addPost(Posts $post): self
     {
         if (!$this->posts->contains($post)) {
             $this->posts[] = $post;
-            $post->setUser($this);
+            $post->setId_user($this);
         }
-
+    
         return $this;
     }
-
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+        $this->commentaires = new ArrayCollection();
+    }
+    
     public function removePost(Posts $post): self
     {
         if ($this->posts->removeElement($post)) {
-            if ($post->getUser() === $this) {
-                $post->setUser(null);
+            // set the owning side to null (unless already changed)
+            if ($post->getId_user() === $this) {
+                $post->setId_user(null);
             }
         }
-
+    
         return $this;
     }
+  
 
- 
-    public function getCommentaires(): Collection
-    {
-        return $this->commentaires;
-    }
-
-    public function addCommentaire(Commentaire $commentaire): self
-    {
-        if (!$this->commentaires->contains($commentaire)) {
-            $this->commentaires[] = $commentaire;
-            $commentaire->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCommentaire(Commentaire $commentaire): self
-    {
-        if ($this->commentaires->removeElement($commentaire)) {
-            if ($commentaire->getUser() === $this) {
-                $commentaire->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-    public function getAdmins(): Collection
-    {
-        return $this->admins;
-    }
-
-    public function addAdmin(Admin $admin): self
-    {
-        if (!$this->admins->contains($admin)) {
-            $this->admins[] = $admin;
-            $admin->setUser($this);
-        }
-        return $this;
-    }
-
-    public function removeAdmin(Admin $admin): self
-    {
-        if ($this->admins->removeElement($admin)) {
-            if ($admin->getUser() === $this) {
-                $admin->setUser(null);
-            }
-        }
-        return $this;
-    }
-
-    // Conducteur relationship methods
-    public function getConducteurs(): Collection
-    {
-        return $this->conducteurs;
-    }
-
-    public function addConducteur(Conducteur $conducteur): self
-    {
-        if (!$this->conducteurs->contains($conducteur)) {
-            $this->conducteurs[] = $conducteur;
-            $conducteur->setUser($this);
-        }
-        return $this;
-    }
-
-    public function removeConducteur(Conducteur $conducteur): self
-    {
-        if ($this->conducteurs->removeElement($conducteur)) {
-            if ($conducteur->getUser() === $this) {
-                $conducteur->setUser(null);
-            }
-        }
-        return $this;
-    }
-
-    // Paiement relationship methods
-    public function getPaiements(): Collection
-    {
-        return $this->paiements;
-    }
-
-    public function addPaiement(Paiement $paiement): self
-    {
-        if (!$this->paiements->contains($paiement)) {
-            $this->paiements[] = $paiement;
-            $paiement->setUser($this);
-        }
-        return $this;
-    }
-
-    public function removePaiement(Paiement $paiement): self
-    {
-        if ($this->paiements->removeElement($paiement)) {
-            if ($paiement->getUser() === $this) {
-                $paiement->setUser(null);
-            }
-        }
-        return $this;
-    }
-
-    // Reservation relationship methods
-    public function getReservations(): Collection
-    {
-        return $this->reservations;
-    }
-
-    public function addReservation(Reservation $reservation): self
-    {
-        if (!$this->reservations->contains($reservation)) {
-            $this->reservations[] = $reservation;
-            $reservation->setUser($this);
-        }
-        return $this;
-    }
-
-    public function removeReservation(Reservation $reservation): self
-    {
-        if ($this->reservations->removeElement($reservation)) {
-            if ($reservation->getUser() === $this) {
-                $reservation->setUser(null);
-            }
-        }
-        return $this;
-    }
-
- 
 }
