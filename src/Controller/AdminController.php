b46e6c5edc03dc258be1366089f99d2ffa3bca58
EYA <mejrieya384@gmail.com>
2025-04-13 17:35:38 +0100

    admin crud

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +20,26 @@ class AdminController extends AbstractController
         return $this->render('back-office/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    #[Route('/admin/users', name: 'admin_users')]
+    public function usersList(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+    
+        return $this->render('back-office/admin/index.html.twig' , [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/user/delete/{id}', name: 'user_delete')]
+    public function deleteUser(User $user, EntityManagerInterface $em): RedirectResponse
+    {
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+
+        return $this->redirectToRoute('admin_users'); 
     }
 } 
